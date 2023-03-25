4250703509b9adf76ffecd19ad8e29bc666b885f
surfzoid <surfzoid@gmail.com>
2023-03-25 12:22:17 +0100

    cosmetic

--- HNEWizard_fr_FR.ts
+++ HNEWizard_fr_FR.ts
@@ -9,209 +9,210 @@
         <translation>Assistant pour HikNetHextractor https://github.com/surfzoid/HikNetExtractor</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="30"/>
+        <location filename="wizard.ui" line="37"/>
         <source>Where to store the python files</source>
         <translation>Où stocker les fichiers python</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="46"/>
+        <location filename="wizard.ui" line="53"/>
         <source>Browse</source>
         <translation>Parcourir</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="81"/>
+        <location filename="wizard.ui" line="88"/>
         <source>Add/Modif</source>
         <translation>Ajouter/Modif</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="94"/>
+        <location filename="wizard.ui" line="101"/>
         <source>Close without saving</source>
         <translation>Fermer sans enregistrer</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="100"/>
+        <location filename="wizard.ui" line="107"/>
         <source>Cancel</source>
         <translation>Annuler</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="116"/>
+        <location filename="wizard.ui" line="123"/>
         <source>Load</source>
         <translation>Charger</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="138"/>
+        <location filename="wizard.ui" line="145"/>
         <source>Device</source>
         <translation>Appareil</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="148"/>
+        <location filename="wizard.ui" line="155"/>
         <source>MyHikCam</source>
         <translation>MonHikCam</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="161"/>
+        <location filename="wizard.ui" line="168"/>
         <source>Ip</source>
         <translation>Ip</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="168"/>
+        <location filename="wizard.ui" line="175"/>
         <source>the ip or domain name of the device</source>
         <translation>l&apos;adresse IP ou le nom de domaine de l&apos;appareil</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="174"/>
+        <location filename="wizard.ui" line="181"/>
         <source>192.168.</source>
         <translation>192.168.</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="184"/>
+        <location filename="wizard.ui" line="191"/>
         <source>Http port</source>
         <translation>port Http</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="191"/>
+        <location filename="wizard.ui" line="198"/>
         <source>Enter the http port for ISAPI</source>
         <translation>Entrez le port http pour ISAPI</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="197"/>
-        <source>800</source>
-        <translation>800</translation>
+        <location filename="wizard.ui" line="204"/>
+        <source>80</source>
+        <oldsource>800</oldsource>
+        <translation>80</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="207"/>
+        <location filename="wizard.ui" line="214"/>
         <source>User</source>
         <translation>Utilisateur</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="214"/>
+        <location filename="wizard.ui" line="221"/>
         <source>Enter the user name</source>
         <translation>Entrez le nom d&apos;utilisateur</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="220"/>
+        <location filename="wizard.ui" line="227"/>
         <source>admin</source>
         <translation>admin</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="233"/>
+        <location filename="wizard.ui" line="240"/>
         <source>Pass</source>
         <translation>Pass</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="240"/>
+        <location filename="wizard.ui" line="247"/>
         <source>Enter the user password</source>
         <translation>Entrez le mot de passe de l&apos;utilisateur</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="246"/>
+        <location filename="wizard.ui" line="253"/>
         <source>pass</source>
         <translation>Pass</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="259"/>
-        <location filename="wizard.ui" line="272"/>
+        <location filename="wizard.ui" line="266"/>
+        <location filename="wizard.ui" line="279"/>
         <source>101 first stream, 102 second stream, etc</source>
         <translation>101 premier flux, 102 deuxième flux, etc</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="265"/>
+        <location filename="wizard.ui" line="272"/>
         <source>Channel</source>
         <translation>Canal</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="278"/>
+        <location filename="wizard.ui" line="285"/>
         <source>101</source>
         <translation>101</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="285"/>
-        <location filename="wizard.ui" line="298"/>
+        <location filename="wizard.ui" line="292"/>
+        <location filename="wizard.ui" line="305"/>
         <source>end with &quot;/&quot;</source>
         <translation>terminer par &quot;/&quot;</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="291"/>
+        <location filename="wizard.ui" line="298"/>
         <source>SavePath</source>
         <translation>SavePath</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="304"/>
+        <location filename="wizard.ui" line="311"/>
         <source>~/</source>
         <translation>~/</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="317"/>
+        <location filename="wizard.ui" line="324"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="327"/>
+        <location filename="wizard.ui" line="334"/>
         <source>DaysToKeep</source>
         <translation>Jours conservès</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="337"/>
+        <location filename="wizard.ui" line="344"/>
         <source>30</source>
         <translation>30</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="344"/>
+        <location filename="wizard.ui" line="351"/>
         <source>if &quot;yes&quot;, UTC time offset of the computer is aply to end and start time of the file name.</source>
         <translation>si &quot;oui&quot;, le décalage horaire UTC de l&apos;ordinateur est appliqué à l&apos;heure de fin et de début du nom de fichier.</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="353"/>
+        <location filename="wizard.ui" line="360"/>
         <source>UtcTOfset</source>
         <translation>UtcTOfset</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="378"/>
+        <location filename="wizard.ui" line="385"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Use Systemd user timer&lt;/p&gt;&lt;p&gt;to execute every each&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Utiliser le minuteur utilisateur Systemd&lt;/p&gt;&lt;p&gt;exécuter chaque&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="398"/>
+        <location filename="wizard.ui" line="405"/>
         <source>minute(s)</source>
         <translation>minute(s)</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="408"/>
+        <location filename="wizard.ui" line="415"/>
         <source>Create/update</source>
         <translation>Créer/mettre à jour</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="418"/>
+        <location filename="wizard.ui" line="425"/>
         <source>Remove</source>
         <translation>Retirer</translation>
     </message>
     <message>
-        <location filename="wizard.ui" line="437"/>
+        <location filename="wizard.ui" line="444"/>
         <source>Duplicate</source>
         <translation>Dupliquer</translation>
     </message>
     <message>
-        <location filename="wizard.cpp" line="33"/>
+        <location filename="wizard.cpp" line="40"/>
         <source>Select Folder to read</source>
         <translation>Sélectionnez le dossier à lire</translation>
     </message>
     <message>
-        <location filename="wizard.cpp" line="154"/>
+        <location filename="wizard.cpp" line="161"/>
         <source>Select Folder to save videos</source>
         <translation>Sélectionnez le dossier pour enregistrer les vidéos</translation>
     </message>
     <message>
-        <location filename="wizard.cpp" line="298"/>
+        <location filename="wizard.cpp" line="311"/>
         <source>Are you sure? </source>
         <translation>Es-tu sûr? </translation>
     </message>
     <message>
-        <location filename="wizard.cpp" line="308"/>
+        <location filename="wizard.cpp" line="324"/>
         <source>New Device</source>
         <translation>Nouvel appareil</translation>
     </message>
     <message>
-        <location filename="wizard.cpp" line="308"/>
+        <location filename="wizard.cpp" line="324"/>
         <source>Enter a new devce name: </source>
         <translation>Saisissez un nouveau nom d&apos;appareil: </translation>
     </message>
